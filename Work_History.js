453704883dc51c1775c0a4409c0dfc07781a4fa4
Kyle Rollins <kyleblankrollins@gmail.com>
2018-04-03 21:00:32 -0600

    Work history update 4.3

--- Work_History.js
+++ Work_History.js
@@ -1,20 +1,41 @@
+// setTimeout(reveal,300);
+//
+// function reveal () {
+//     var jobDesc = document.getElementsByClassName('jobDesc')[0];
+//
+//     document.getElementsByClassName('dropDownBox')[0].onclick = function() {
+//       if(this.innerHTML === '▼')
+//       {
+//         this.innerHTML = '▲';
+//         jobDesc.classList.add('jobDescShow');
+//       } else {
+//         this.innerHTML = '▼';
+//         jobDesc.classList.remove('jobDescShow');
+//       }
+//     }
+// }
+function reveal1() {
+    var description = document.getElementById("description1");
+    description.style.display="block";
+}
 
-setTimeout(reveal,300);
+function reveal2() {
+    var description = document.getElementById("description2");
+    description.style.display="block";
+}
 
-function reveal () {
-    var jobDesc = document.getElementsByClassName('jobDesc')[0];
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    var rNameDrop = document.getElementById("rNameDrop");
+    var lNameDrop = document.getElementById("lNameDrop");
+    if (!e.target.matches('.dropBtn')) {
 
-    document.getElementsByClassName('dropDownBox')[0].onclick = function() {
-      if(this.innerHTML === '▼')
-      {
-        this.innerHTML = '▲';
-        jobDesc.classList.add('jobDescShow');
-      } else {
-        this.innerHTML = '▼';
-        var computedStyle = window.getComputedStyle(jobDesc),
-            marginLeft = computedStyle.getPropertyValue('margin-left');
-        jobDesc.style.marginLeft = marginLeft;
-        jobDesc.classList.remove('jobDescShow');
-      }
+        if (lNameDrop.style.display="block"){
+            lNameDrop.style.display="none"
+        }
+
+        if (rNameDrop.style.display="block"){
+            rNameDrop.style.display="none"
+        }
     }
 }
