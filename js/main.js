54535545d568835b9049f59f98191a9e59987673
Kyle Rollins <kyleblankrollins@gmail.com>
2018-04-11 21:53:02 -0600

    4.11 update
    Slide reveal is working! The older method works. It's not elegant, but I can refine it later.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,121 @@
-var samples = document.getElementById("samples");
-var techExpand = document.getElementById("techExpand");
-var designExpand = document.getElementById("designExpand");
-var pubExpand = document.getElementById("pubExpand");
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-techExpand.classList.remove("panel");
-designExpand.classList.add("hide");
-pubExpand.classList.add("hide");
+    var techWriting = document.getElementById("techWriting");
+    var docDesign = document.getElementById("docDesign");
+    var pubContent = document.getElementById("pubContent");
+    var samples = document.getElementById("samples");
+    var sampleTech = document.getElementById("sampleTech");
+    var sampleDesign = document.getElementById("sampleDesign");
+    var sampleArticles = document.getElementById("sampleArticles");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxWidth){
-      panel.style.maxWidth = null;
-    } else {
-      panel.style.maxWidth = "90%";
+    docDesign.classList.add("collapse");
+    pubContent.classList.add("collapse");
+    sampleTech.style.cssText = "display: grid !important";
+
+    techWriting.addEventListener('click', widthToggle);
+    docDesign.addEventListener('click', widthToggle);
+    pubContent.addEventListener('click', widthToggle);
+
+    function widthToggle(event) {
+
+        var elParent = event.target.parentElement;
+        var el = event.target;
+        var techClassList = techWriting.classList;
+        var designClassList = docDesign.classList;
+        var pubClassList = pubContent.classList;
+
+        if (elParent.id === techWriting.id || el.id === techExpand.id) {
+
+            if (techWriting.className.match("collapse")) {
+                techClassList.add("expand");
+                techClassList.remove("collapse");
+                designClassList.add("collapse");
+                designClassList.remove("expand");
+                pubClassList.add("collapse");
+                pubClassList.remove("expand");
+                sampleTech.style.cssText = "display: grid !important";
+                sampleDesign.style.cssText = "display: none !important";
+                sampleArticles.style.cssText = "display: none !important";
+            } else {
+                techClassList.add("collapse");
+                techClassList.remove("expand");
+                sampleTech.style.cssText = "display: none !important";
+            }
+
+        }
+        else if (elParent.id === docDesign.id  || el.id === designExpand.id) {
+
+            if (docDesign.className.match("collapse")) {
+                designClassList.add("expand");
+                designClassList.remove("collapse");
+                techClassList.add("collapse");
+                techClassList.remove("expand");
+                pubClassList.add("collapse");
+                pubClassList.remove("expand");
+                sampleDesign.style.cssText = "display: grid !important";
+                sampleTech.style.cssText = "display: none !important";
+                sampleArticles.style.cssText = "display: none !important";
+            } else {
+                designClassList.add("collapse");
+                designClassList.remove("expand");
+                sampleDesign.style.cssText = "display: none !important";
+            }
+
+        } else if (elParent.id === pubContent.id  || el.id === pubExpand.id) {
+
+            if (pubContent.className.match("collapse")) {
+                pubClassList.add("expand");
+                pubClassList.remove("collapse");
+                techClassList.add("collapse");
+                techClassList.remove("expand");
+                designClassList.add("collapse");
+                designClassList.remove("expand");
+                sampleArticles.style.cssText = "display: grid !important";
+                sampleTech.style.cssText = "display: none !important";
+                sampleDesign.style.cssText = "display: none !important";
+            } else {
+                pubClassList.add("collapse");
+                pubClassList.remove("expand");
+                sampleArticles.style.cssText = "display: none !important";
+            }
+
+        }
+
+
+        // if (elParent.id === techWriting.id || el.id === techExpand.id) {
+        //     techWriting.classList.add("expand");
+        //     techWriting.classList.remove("collapse");
+        //     sampleTech.classList.remove("hide");
+        //     sampleDesign.classList.add("hide");
+        //     sampleArticles.classList.add("hide");
+        //     techOverlay.classList.remove("overlayColumnCollapse");
+        //     docDesign.classList.add("collapse");
+        //     docDesign.classList.remove("expand");
+        //     pubContent.classList.add("collapse");
+        //     pubContent.classList.remove("expand");
+        //     console.log(techWriting.classList);
+        // } else if (elParent.id === docDesign.id  || el.id === designExpand.id) {
+        //     techWriting.classList.add("collapse");
+        //     techWriting.classList.remove("expand");
+        //     sampleTech.classList.add("hide");
+        //     sampleDesign.classList.remove("hide");
+        //     sampleArticles.classList.add("hide");
+        //     techOverlay.classList.add("overlayColumnCollapse");
+        //     docDesign.classList.add("expand");
+        //     docDesign.classList.remove("collapse");
+        //     pubContent.classList.add("collapse");
+        //     pubContent.classList.remove("expand");
+        //     console.log(docDesign.classList);
+        // } else if (elParent.id === pubContent.id  || el.id === pubExpand.id) {
+        //     techWriting.classList.add("collapse");
+        //     techWriting.classList.remove("expand");
+        //     sampleTech.classList.add("hide");
+        //     sampleDesign.classList.add("hide");
+        //     sampleArticles.classList.remove("hide");
+        //     techOverlay.classList.add("overlayColumnCollapse");
+        //     docDesign.classList.add("collapse");
+        //     docDesign.classList.remove("expand");
+        //     pubContent.classList.add("expand");
+        //     pubContent.classList.remove("collapse");
+        //     console.log(pubContent.classList);
+        // }
     }
-  });
-}
-
-if(document.readyState === 'complete') {
-    function getSiblings() {
-        var siblings = samples.parentNode.childNodes;
-
-        console.log(siblings);
-    }
-}
