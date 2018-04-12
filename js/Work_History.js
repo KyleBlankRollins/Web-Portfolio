db9e021000233bfd607bd3e96229258bdb52c3af
Kyle Rollins <krollins@mmm.com>
2018-04-12 17:04:54 -0600

    4.12 update 2

--- js/Work_History.js
+++ js/Work_History.js
@@ -1,29 +1,28 @@
 
-function hideDescriptions () {
-    var jobDesc = document.getElementsByClassName("jobDesc")[0];
-    var i;
+var jobDesc = document.getElementsByClassName("jobDesc");
+var i;
 
-    for (i = 0; i < acc.length; i++) {
-        this.classList.add("hide");
+console.log(jobDesc);
+
+for (i = 0; i < jobDesc.length; i++) {
+    jobDesc[i].addEventListener("click", function() { // TODO: need to add an event for "onLoad" or similar that invokes the function when the document is ready.
+        this.classList.add("hide"); // REVIEW: may need to add new hide and show classes for jobDesc.
     }
 }
 
-
-jobDesc.classList.add("hide");
-
-function heightToggle () {
-    var acc = document.getElementsByClassName("jobTitle");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxWidth){
-          panel.style.maxWidth = null;
-        } else {
-          panel.style.maxWidth = "90%";
-        }
-      });
-    }
-}
+// function heightToggle () { // TODO: modify this so that on click, jobDesc show or hide classes are added.
+//     var acc = document.getElementsByClassName("jobTitle");
+//     var i;
+//
+//     for (i = 0; i < acc.length; i++) {
+//       acc[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxWidth){
+//           panel.style.maxWidth = null;
+//         } else {
+//           panel.style.maxWidth = "90%";
+//         }
+//       });
+//     }
+// }
