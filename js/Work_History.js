aa13db69a5286e9073456b4f06be4a6aa36c270c
Kyle Rollins <kyleblankrollins@gmail.com>
2018-04-12 21:39:39 -0600

    4.12 final update
    Job history is working.

--- js/Work_History.js
+++ js/Work_History.js
@@ -1,28 +1,68 @@
 
+var jobTitle1 = document.getElementById('jobTitle1');
+var jobDesc1 = document.getElementById("jobDesc1");
+var jobTitle2 = document.getElementById('jobTitle2');
+var jobDesc2 = document.getElementById("jobDesc2");
+var jobTitle3 = document.getElementById('jobTitle3');
+var jobDesc3 = document.getElementById("jobDesc3");
+var jobTitle4 = document.getElementById('jobTitle4');
+var jobDesc4 = document.getElementById("jobDesc4");
+var jobTitle5 = document.getElementById('jobTitle5');
+var jobDesc5 = document.getElementById("jobDesc5");
+var jobTitle6 = document.getElementById('jobTitle6');
+var jobDesc6 = document.getElementById("jobDesc6");
+var jobTitle = document.getElementsByClassName("jobTitle");
 var jobDesc = document.getElementsByClassName("jobDesc");
-var i;
 
-console.log(jobDesc);
+jobDesc1.classList.add('hide');
+jobDesc2.classList.add('hide');
+jobDesc3.classList.add('hide');
+jobDesc4.classList.add('hide');
+jobDesc5.classList.add('hide');
+jobDesc6.classList.add('hide');
 
-for (i = 0; i < jobDesc.length; i++) {
-    jobDesc[i].addEventListener("click", function() { // TODO: need to add an event for "onLoad" or similar that invokes the function when the document is ready.
-        this.classList.add("hide"); // REVIEW: may need to add new hide and show classes for jobDesc.
-    }
+for (i = 0; i < jobTitle.length; i++) {
+  jobTitle[i].addEventListener("click", toggleVisibility)
 }
 
-// function heightToggle () { // TODO: modify this so that on click, jobDesc show or hide classes are added.
-//     var acc = document.getElementsByClassName("jobTitle");
-//     var i;
-//
-//     for (i = 0; i < acc.length; i++) {
-//       acc[i].addEventListener("click", function() {
-//         this.classList.toggle("active");
-//         var panel = this.nextElementSibling;
-//         if (panel.style.maxWidth){
-//           panel.style.maxWidth = null;
-//         } else {
-//           panel.style.maxWidth = "90%";
-//         }
-//       });
-//     }
-// }
+for (i = 0; i < jobDesc.length; i++) {
+  jobDesc[i].addEventListener("click", toggleVisibility)
+}
+
+function toggleVisibility(id) {
+
+    var el = event.target;
+    var elParent = el.parentElement;
+
+    console.log(elParent);
+
+    if (el.id == "jobTitle1" || elParent.id == "jobDesc1" || elParent.id == "horizontalUL") {
+        jobDesc1.classList.toggle('hide');
+        jobTitle1.classList.toggle('titleMove');
+    }
+
+    if (el.id == "jobTitle2" || elParent.id == "jobDesc2" || elParent.id == "horizontalUL") {
+        jobDesc2.classList.toggle('hide');
+        jobTitle2.classList.toggle('titleMove');
+    }
+
+    if (el.id == "jobTitle3" || elParent.id == "jobDesc3" || elParent.id == "horizontalUL") {
+        jobDesc3.classList.toggle('hide');
+        jobTitle3.classList.toggle('titleMove');
+    }
+
+    if (el.id == "jobTitle4" || elParent.id == "jobDesc4" || elParent.id == "horizontalUL") {
+        jobDesc4.classList.toggle('hide');
+        jobTitle4.classList.toggle('titleMove');
+    }
+
+    if (el.id == "jobTitle5" || elParent.id == "jobDesc5" || elParent.id == "horizontalUL") {
+        jobDesc5.classList.toggle('hide');
+        jobTitle5.classList.toggle('titleMove');
+    }
+
+    if (el.id == "jobTitle6" || elParent.id == "jobDesc6" || elParent.id == "horizontalUL") {
+        jobDesc6.classList.toggle('hide');
+        jobTitle6.classList.toggle('titleMove');
+    }
+}
