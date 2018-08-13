00d07976b5d126867ec77d1479d55458897364a2
KyleBlankRollins <kyleblankrollins@gmail.com>
2018-08-13 10:08:32 -0600

    Initialization

---
+++ README.md
@@ -0,0 +1,27 @@
+# Using Vue with Airtable's Study Guide Template
+
+Project Walkthrough [here](http://patrickwho.me/tutorial/integrate-airtable-study-guide-template-with-vue-js/).
+
+This is just a simple project to help others get more familiar with using Vue and Airtable.
+
+Get some experience with the following:
+
+* v-on:change / @change event
+* v-for and :key
+* v-bind / :
+* Using Axios to get, post, put and delete records.
+* Computed properties and how they relate to data properties.
+
+## About Me - Junior Dev for Life
+
+I learn best by looking at the code others have written and building things.
+
+In my own learning journey I have often been frustrated with the lack of simple, to-the-point code examples. 
+
+Rather, there seems to be an over-abundance of lengthy tutorials that often teach too many things to absorb effectively.
+
+The short projects I publish are my attempt to provide succinct illustrations of a handful of new concepts along with a few ideas on how to practice.
+
+Any and all constructive feedback and requests are very welcome.
+
+[Patrick](http://patrickwho.me)
--- main.js
+++ main.js
@@ -1,101 +1,149 @@
-// const store = new Vuex.Store({
-//     state: {
-//         showGalMenu: false,
-//         showColMenu: false,
-//
-//         galUnits: [
-//             { id: 'gal1', title: 'Assault Railgun', desc: 'Long-range, medium attack.', speed: 100, armor: 6, damage: 50, rof: 'High', hull: 550, strong: 'Armored, Railguns', weak: 'Railguns, Aircraft, Cruisers', image: 'assaultRailgun_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal2', title: 'Assault Ship', desc: 'This is an assault ship.', speed: 70, armor: 6, damage: 24, rof: 'Medium', hull: 1400, strong: 'Strikecraft', weak: 'Railguns, Cruisers', image: 'assaultShip_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal3', title: 'Baserunner', desc: 'It runs bases. Like in baseball.', speed: 70, armor: 2, damage: 5, rof: 'Low', hull: 3350, strong: 'N/A', weak: 'Everything', image: 'baserunner_gaalsien.jpg', current: false,  speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal4', title: 'Carrier', desc: 'Carries stuff. Like other ships.', speed: 10, armor: 40, damage: 10, rof: 'Very low', hull: 12500, strong: 'N/A', weak: 'Railguns, Cruisers', image: 'carrier_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal5', title: 'Heavy Railgun', desc: 'Long-range, heavy-weapon platform.', speed: 62, armor: 3, damage: 225, rof: 'Medium', hull: 620, strong: 'Armored, Railguns', weak: 'Railguns, Aircraft, Cruisers', image: 'heavyRailgun_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal6', title: 'Honorguard Cruiser', desc: 'Heavy assault cruiser.', speed: 60, armor: 6, damage: 400, rof: 'Medium', hull: 3600, strong: 'Ground units', weak: 'Aircraft, railguns', image: 'honorguardCruiser_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal7', title: 'Interceptor', desc: 'Air superiority unit.', speed: 435, armor: 1, damage: 150, rof: 'Medium', hull: 475, strong: 'Ground', weak: 'Anti-air, Interceptors, Strikecraft', image: 'interceptor_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal8', title: 'Missile Ship', desc: 'Launches missiles at air units.', speed: 55, armor: 5, damage: 250, rof: 'Low', hull: 1800, strong: 'Aircraft', weak: 'Ground units', image: 'missileShip_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal9', title: 'Precision Bomber', desc: 'Drops big bombs that make big explosions.', speed: 550, armor: 15, damage: 1000, rof: 'Low', hull: 1250, strong: 'Ground', weak: 'Anti-air, Interceptors, Strikecraft', image: 'precisionBomber_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal10', title: 'Production Cruiser', desc: 'Produces smaller units.', speed: 70, armor: 5, damage: 20, rof: 'Low', hull: 4100, strong: 'N/A', weak: 'Everything', image: 'productionCruiser_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal11', title: 'Salvager', desc: 'Uses its tools to salvage stuff.', speed: 75, armor: 2, damage: 0, rof: 'N/A', hull: 900, strong: 'N/A', weak: 'Everything', image: 'salvager_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'gal12', title: 'Sandskimmer', desc: 'Light attack vehicle.', speed: 130, armor: 0, damage: 15, rof: 'Medium', hull: 650, strong: 'Strikecraft, Railguns', weak: 'AAVs, Cruisers', image: 'sandskimmer_gaalsien.jpg', current: true, speedHigher: true, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: true },
-//
-//             { id: 'gal13', title: 'Siege Cruiser', desc: 'Lobs missiles at unsuspecting targets.', speed: 55, armor: 3, damage: 100, rof: 'Low', hull: 2500, strong: 'Ground', weak: 'Strikecraft, Railguns', image: 'siegeCruiser_gaalsien.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//         ],
-//         colUnits: [
-//             { id: 'col1', title: 'AAV', desc: 'Armored assault vehicle.', speed: 80, armor: 8, damage: 11, rof: 'Very high', hull: 1400, strong: 'Strikecraft', weak: 'Railguns, Cruisers', image: 'AAV_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col2', title: 'Artillery Cruiser', desc: 'Smashes things with missles.', speed: 50, armor: 5, damage: 100, rof: 'Low', hull: 2500, strong: 'Ground', weak: 'Strikecraft, Railguns', image: 'artilleryCruiser_coalition.jpg', current: false,  speedHigher: false, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: false },
-//
-//             { id: 'col3', title: 'Assault Cruiser', desc: 'Assaults big stuff.', speed: 65, armor: 15, damage: 70, rof: 'High', hull: 2700, strong: 'Ground', weak: 'Aircraft, Railguns', image: 'assaultCruiser_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col4', title: 'Baserunner', desc: 'Builds stuff.', speed: 65, armor: 2, damage: 5, rof: 'Medium', hull: 3350, strong: 'N/A', weak: 'Everything', image: 'baserunner_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col5', title: 'Battlecruiser', desc: 'Big ship. Big guns.', speed: 50, armor: 25, damage: 300, rof: 'Low', hull: 6500, strong: 'Ground', weak: 'Railguns, Aircraft', image: 'battleCruiser_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col6', title: 'Carrier', desc: 'Carries things. Lots of things.', speed: 60, armor: 25, damage: 10, rof: 'Very low', hull: 15000, strong: 'N/A', weak: 'Railguns, Cruisers', image: 'carrier_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col7', title: 'Gunship', desc: 'Flying ship with a big gun.', speed: 375, armor: 10, damage: 15, rof: 'Very high', hull: 1050, strong: 'Ground', weak: 'Anti-air, Interceptors, Strikecraft', image: 'gunship_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col8', title: 'LAV', desc: 'Light attack vehicle.', speed: 110, armor: 0, damage: 15, rof: 'Medium', hull: 550, strong: 'Strikecraft, Railguns', weak: 'Assault Railguns, Cruisers', image: 'LAV_coalition.jpg', current: true, speedHigher: false, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: false },
-//
-//             { id: 'col9', title: 'Missile Battery', desc: 'Shoots missiles at air units.', speed: 50, armor: 5, damage: 250, rof: 'Low', hull: 1800, strong: 'Aircraft', weak: 'Ground units', image: 'missileBattery_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col10', title: 'Railgun', desc: 'Long-range, heavy-weapon platform.', speed: 66, armor: 3, damage: 225, rof: 'Medium', hull: 675, strong: 'Armored, Railguns', weak: 'Armored, Railguns', image: 'railgun_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col11', title: 'Salvager', desc: 'Uses its tools to salvage stuff.', speed: 75, armor: 5, damage: 0, rof: 'N/A', hull: 900, strong: 'N/A', weak: 'Everything', image: 'salvager_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col12', title: 'Strike Fighter', desc: 'Fast air superiority unit.', speed: 435, armor: 1, damage: 120, rof: 'Low', hull: 650, strong: 'Ground', weak: 'Anti-air, Interceptors, Strikecraft', image: 'strikeFighter_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col13', title: 'Support Cruiser', desc: 'Large support unit.', speed: 70, armor: 2, damage: 1, rof: 'Low', hull: 4100, strong: 'N/A', weak: 'Everything', image: 'supportCruiser_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//
-//             { id: 'col14', title: 'Tactical Bomber', desc: 'Drops big bombs that make big explosions.', speed: 370, armor: 15, damage: 2500, rof: 'Low', hull: 1050, strong: 'Ground', weak: 'Anti-air, Interceptors, Strikecraft', image: 'tacticalBomber_coalition.jpg', current: false, speedHigher: null, armorHigher: null, damageHigher: null, rofHigher: null, hullHigher: null },
-//         ],
-//     },
-//     mutations: {
-//
-//     }
-// })
+const airtableKey = 'keyJqGMqnXkEmBt61'
 
-var galUnits = Vue.component('gaalsien-units', {
-    template: '<div> <p class="unit-description"> {{ desc }} </p> <slot></slot> </div>',
-});
-
-var colUnits = Vue.component('coalition-units', {
-    template: '<div> <p class="unit-description"> {{ desc }} </p>  <slot></slot> </div>',
-});
-
-var vm = new Vue({
-    el: '#app',
-    mounted () {
-        axios
-            .get('galUnitData.json')
-            .then(response => (this.galUnits = JSON.parse(response))) //JSON.parse doesn't work here.
-            .get('colUnitData.json')
-            .then(response => (this.colUnits = JSON.parse(response)))
+new Vue({
+    name: 'MainApp',
+    el: "#app",
+    data: {
+        name: '',
+        note: '',
+        studyCards: []
     },
-    data () {
-            return {
-                galUnits: [],
-                colUnits: [],
-            }
+    computed: {
+        memorizedCards() {
+            return this.studyCards.filter((card) => {
+                return card.fields.Memorized
+            })
         },
-    components: {
-        'galUnits': galUnits,
-        'colUnits': colUnits,
+        unMemorizedCards() {
+            return this.studyCards.filter((card) => {
+                return !card.fields.Memorized
+            })
+        }
     },
+    created,
     methods: {
-        parseGalJSON: function() {
-            var galJSON = JSON.parse(galUnits);
-            console.log(galJSON);
+        saveStudyCard,
+        updateCardStatus,
+        deleteCard
+
+    }
+})
+
+/**
+ * Step 1: Created Life Cycle Hook
+ *
+ * Grab our current studyCards on startup and save them to the 'studyCards' array on our 'data' property.
+ */
+function created() {
+    axios.get(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards?maxRecords=20&view=Main%20View&api_key=${airtableKey}`)
+        .then((resp) => {
+            console.log('response', resp)
+
+            // 1-1 If we retrieve records successfully, add the to our studyCards list.
+            if (resp.status === 200 && resp.data.records.length > 0) {
+                this.studyCards = resp.data.records
+            } else {
+                console.error('Unable to retrieve study cards. Please refresh the page and try again.')
+            }
+        })
+}
+
+/**
+ * Step 2: Save a New Card to Airtable
+ */
+function saveStudyCard() {
+
+    // 2-1 return early if either of the required fields are empty
+    if (!this.name || !this.note) {
+        return
+    }
+
+    // 2-2 Create the payload object as stated in Airtable's api documentation
+    const payload = {
+        fields: {
+            Name: this.name,
+            Notes: this.note,
+            // Memorized: false,
+            Attachments: []
         }
     }
-});
+
+    // 2-3 Send the data
+    axios.post(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards?api_key=${airtableKey}`, payload)
+        .then((resp) => {
+
+            if (resp.status === 200 && resp.data.id) {
+
+                // 2-4 If we're successful, update the studyCards array
+                this.studyCards.push(resp.data)
+
+                // 2-5 clear the inputs.
+                this.name = ''
+                this.note = ''
+
+                // 2-6 Material Lite isn't reactive, so we're going to manually remove the css class so our label re-appear YUCK
+                document.getElementById('studyCardNameInput').classList.remove('is-dirty')
+                document.getElementById('studyCardNoteInput').classList.remove('is-dirty')
+            } else {
+                // handle the error - not something we're doing now.
+                console.error('Unable to save card.', payload)
+            }
+        })
+}
+
+/**
+ * Step 3: Update Card Status - Memorized is TRUE or FALSE
+ *
+ * @param (object) studyCard According to the API documentation, the entire studyCard object is required for a put.
+ */
+function updateCardStatus(studyCard) {
+
+    // 3-1 Create the payload object.
+    const payload = {
+        fields: studyCard.fields
+    }
+
+    // 3-2 PUT request to the db to update the studyCard found at studyCard.id
+    axios.put(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards/${studyCard.id}?api_key=${airtableKey}`, payload)
+        .then((resp) => {
+
+            if (resp.status === 200 && resp.data.id) {
+
+                // 3-3 Find the updated card in our array and replace it.
+                this.studyCards = this.studyCards.map((card) => {
+                    if (resp.data.id === card.id) {
+                        card = resp.data
+                    }
+
+                    return card
+                })
+            } else {
+                // handle the error - not something we're doing now.
+                console.error('Unable to update card.', payload)
+            }
+
+        })
+}
+
+/**
+ * Step 4: Delete a Card
+ *
+ * @param {number} studyCardID The .id for the card.
+ */
+function deleteCard(studyCardID) {
+
+    // 4-1 Send the DELETE request for studyCardID
+    axios.delete(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards/${studyCardID}?api_key=${airtableKey}`)
+        .then((resp) => {
+
+            // 4-2 If the delete is successful we'll filter the delete card from our studyCards array.
+            if (resp.status === 200 && resp.data.deleted === true) {
+                this.studyCards = this.studyCards.filter((card) => {
+                    return card.id !== studyCardID
+                })
+            } else {
+                // handle the error - not something we're doing now.
+                console.error('Unable to delete card.')
+            }
+        })
+}
