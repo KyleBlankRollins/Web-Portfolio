c6a8a50526e1b9f5953e184598bfb0cb8e6d765d
Kyle Rollins <kyleblankrollins@gmail.com>
2021-03-21 15:51:52 -0500

    Various fixes for final version
    + Added README.md
    + Added links to sample PDFs.
    + Re-factored common colors into global custom properties

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
+// (function () {
+//   window.addEventListener("DOMContentLoaded", (event) => {
+//     let tocItems = document.querySelectorAll(".toc-item");
+
+//     addEventListeners(tocItems);
+//   });
+// })();
+
 function moveDivider() {
   let divider = document.querySelector(".divider");
   let slider = document.querySelector(".slider");
   divider.style.width = slider.value + "%";
 }
+
+// function checkHash(event) {
+//   let hash = window.location.hash;
+
+//   console.log(event);
+//   console.log(hash);
+// }
+
+// function addEventListeners(tocItems) {
+//   tocItems.forEach((item) => {
+//     item.addEventListener("click", () => {
+//       console.log(item);
+//     });
+//   });
+// }
