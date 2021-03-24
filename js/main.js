d45c3f5ffdc49bf9054c98c9035d3d08818c8efd
Kyle Rollins <kyleblankrollins@gmail.com>
2021-03-23 22:02:33 -0500

    Mobile look set

--- js/main.js
+++ js/main.js
@@ -1,28 +1,169 @@
-// (function () {
-//   window.addEventListener("DOMContentLoaded", (event) => {
-//     let tocItems = document.querySelectorAll(".toc-item");
+let appUI = {
+  header: "",
+  mainNav: "",
+  navWrapper: "",
+  subNavItems: "",
+  navLinks: "",
+  navToggle: "",
+  imageDivider: "",
+  imageSlider: "",
+};
 
-//     addEventListeners(tocItems);
-//   });
-// })();
+(function () {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    // Initialize UI elements
+    appUI.header = document.querySelector(".a11y-nav");
+    appUI.mainNav = document.querySelector(".nav-main");
+    appUI.navWrapper = document.querySelector(".nav-wrapper");
+    appUI.subNavItems = [...document.querySelectorAll(".has-subnav")];
+    appUI.navLinks = [...document.querySelectorAll(".nav-main > ul > li > a")];
+    appUI.navToggle = document.querySelector(".nav-toggle");
+    appUI.divider = document.querySelector(".divider");
+    appUI.slider = document.querySelector(".slider");
 
-function moveDivider() {
-  let divider = document.querySelector(".divider");
-  let slider = document.querySelector(".slider");
-  divider.style.width = slider.value + "%";
+    // After UI elements set, initialize app logic
+    initializeApp();
+  });
+})();
+
+function initializeApp() {
+  appUI.header.classList.add("has-js-loaded");
+  appUI.subNavItems.forEach((subNavItem) => setupSubNavItem(subNavItem));
+  appUI.navLinks.forEach((navLink) => setupNavLink(navLink));
+  setupNavToggle(appUI.navToggle, appUI.navWrapper);
 }
 
-// function checkHash(event) {
-//   let hash = window.location.hash;
+// Responsive and accessible navigation
+function navToggleClickEventHandler(event) {
+  toggleMainNav(this);
+}
 
-//   console.log(event);
-//   console.log(hash);
-// }
+function navToggleKeydownHandler(event) {
+  const toggleButton = event.target;
 
-// function addEventListeners(tocItems) {
-//   tocItems.forEach((item) => {
-//     item.addEventListener("click", () => {
-//       console.log(item);
-//     });
-//   });
-// }
+  switch (event.key) {
+    // Actual button, so Spacebar is captured already...
+
+    case "Esc":
+    case "Escape":
+      closeMainNav(toggleButton);
+      break;
+    default:
+      return;
+  }
+}
+
+function subNavClickEventHandler(event) {
+  const subNav = this.parentNode.querySelector(".subnav");
+
+  if (subNav !== null) {
+    event.preventDefault();
+    toggleSubNav(this, subNav);
+  }
+}
+
+function subNavKeydownHandler(event) {
+  let subNav;
+  let currentNavItem;
+
+  if (this.classList.contains(".subNav")) {
+    subNav = this;
+    currentNavItem = this.querySelector("a");
+  } else {
+    subNav = this.parentNode.querySelector(".subnav");
+    currentNavItem = event.target;
+  }
+
+  switch (event.key) {
+    case "Spacebar":
+    case " ":
+      // Open subnav and jump to first item
+      toggleSubNav(currentNavItem, subNav);
+      break;
+    case "Esc":
+    case "Escape":
+      closeSubNav(currentNavItem, subNav);
+      break;
+    default:
+      return;
+  }
+}
+
+// Handle image comparison
+function moveDivider() {
+  appUI.divider.style.width = appUI.slider.value + "%";
+}
+
+function closeMainNav(button) {
+  button.setAttribute("aria-expanded", "false");
+  appUI.navWrapper.setAttribute("aria-hidden", "true");
+  appUI.navWrapper.classList.add("noshow");
+}
+
+function openMainNav(button) {
+  button.setAttribute("aria-expanded", "true");
+  appUI.navWrapper.setAttribute("aria-hidden", "false");
+  appUI.navWrapper.classList.remove("noshow");
+}
+
+function toggleMainNav(button) {
+  if (button.getAttribute("aria-expanded") === "true") {
+    closeMainNav(button);
+  } else {
+    openMainNav(button);
+  }
+}
+
+function closeSubNav(link, subNav) {
+  link.setAttribute("aria-expanded", "false");
+  subNav.setAttribute("aria-hidden", "true");
+  subNav.classList.add("noshow");
+}
+
+function openSubNav(link, subNav) {
+  link.setAttribute("aria-expanded", "true");
+  subNav.setAttribute("aria-hidden", "false");
+  subNav.classList.remove("noshow");
+}
+
+function toggleSubNav(link, subNav) {
+  if (subNav !== null) {
+    if (link.getAttribute("aria-expanded") === "true") {
+      closeSubNav(link, subNav);
+    } else {
+      openSubNav(link, subNav);
+    }
+  }
+}
+
+function setupSubNavItem(subNavItem) {
+  let link = subNavItem.querySelector("a");
+  let subNav = subNavItem.querySelector(".subnav");
+
+  link.setAttribute("aria-expanded", "false");
+  link.setAttribute("role", "button");
+  if (subNav) {
+    subNav.removeAttribute("hidden");
+    subNav.setAttribute("aria-hidden", "true");
+    subNav.classList.add("noshow");
+  }
+  // subNav.setAttribute('aria-label', link.innerText + " submenu");
+  // subNav.setAttribute('tabindex', '-1');
+}
+
+function setupNavLink(navLink) {
+  navLink.addEventListener("click", subNavClickEventHandler);
+  navLink.parentNode.addEventListener("keydown", subNavKeydownHandler);
+}
+
+function setupNavToggle(navToggle, navWrapper) {
+  navToggle.setAttribute("aria-expanded", "false");
+  navWrapper.classList.add("noshow");
+
+  if (window.screen.width > 768) {
+    navWrapper.classList.remove("noshow");
+  }
+
+  navToggle.addEventListener("click", navToggleClickEventHandler);
+  navToggle.addEventListener("keydown", navToggleKeydownHandler);
+}
